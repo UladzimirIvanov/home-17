27be741ec5c7713ad839e88bf8ff8e3f94c6a472
UladzimirIvanov <ivanouum@gmail.com>
2024-05-21 17:05:50 +0300

    Result Task1

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,2 +1,14 @@
-package Task1;public class Main {
+package Task1;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите дату рождения в формате yyyy-MM-dd");
+        LocalDate dateUserBirthday = LocalDate.parse(scanner.next());
+        //System.out.println("You Birthday: " + dateUserBirthday);
+        System.out.println("Вам исполнится 100 лет: " + dateUserBirthday.plusYears(100));
+    }
 }
